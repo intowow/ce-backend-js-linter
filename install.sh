632e16b27ef1ff97eeec616529564a9d2af24dfe
Yuxun <yuxun@intowow.com>
2017-07-04 17:34:32 +0800

    Fix workspace not found issue

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 WORKSPACE=`pwd`
 
-CONFIG_URL="https://raw.github.com/intowow/ce-backend-js-linter/feature/auto_install_linter/eslintrc.json"
-PRECOMMIT_SCRIPTS="https://raw.github.com/intowow/ce-backend-js-linter/feature/auto_install_linter/git_hooks/pre-commit"
+CONFIG_URL="https://raw.github.com/intowow/ce-backend-js-linter/master/eslintrc.json"
+PRECOMMIT_SCRIPTS="https://raw.github.com/intowow/ce-backend-js-linter/master/git_hooks/pre-commit"
 
 if [ ! -d "${WORKSPACE}/.git" ];
 then
-  echo -e "\033[41;37mAbort\!\! Is this a valid git repository folder?\033[0m"
+  echo -e "\033[41;37mFailed to install. Is this a valid git repository folder?\033[0m"
   exit 1
 fi
 
 # Download config
-wget ${CONFIG_URL} -O ${WORKSPACE}/.eslinter.yaml
+wget ${CONFIG_URL} -O ${WORKSPACE}/.eslinter.json
 
 # Download hook scripts
-HOOK_DIR=${WORK_SPACE}/.git/hooks
+HOOK_DIR=${WORKSPACE}/.git/hooks
 wget ${PRECOMMIT_SCRIPTS} -P ${HOOK_DIR}/
 
 # Make script executable
